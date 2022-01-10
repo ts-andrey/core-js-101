959515a89736464c990188e1b892b30b0b63b325
Andrei Tsakunou <andrey.tsakunov@gmail.com>
2022-01-10 20:44:50 +0300

    feat: add one more function task implementation 2022-01-10 (20:44:50)

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -151,8 +151,10 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return function combiner(...args2) {
+    return fn(...args1.concat(args2));
+  };
 }
 
 /**
